1b939aca4502ab34c3a532f9d4ddb000fab4bc5a
lincyaw <814750204@qq.com>
2024-06-22 11:44:39 +0800

    fix some code issue

--- src/otelproto4research/read.py
+++ src/otelproto4research/read.py
@@ -1,27 +1,36 @@
+from typing import List, Dict, Any
 import opentelemetry.proto as op
 import opentelemetry.proto.metrics.v1.metrics_pb2 as metrics_pb2
 import opentelemetry.proto.trace.v1.trace_pb2 as trace_pb2
 import opentelemetry.proto.logs.v1.logs_pb2 as logs_pb2
+import opentelemetry.proto.common.v1.common_pb2 as common_pb2
 import pandas as pd
 
-def __parse_attributes(attributes: List[metrics_pb2.KeyValue]) -> Dict[str, Any]:
+
+def __parse_attributes(attributes: List[common_pb2.KeyValue]) -> Dict[
+    str, Any]:
     return {attr.key: attr.value for attr in attributes}
 
-def __parse_number_data_point(point: metrics_pb2.NumberDataPoint) -> Dict[str, Any]:
+
+def __parse_number_data_point(point: metrics_pb2.NumberDataPoint) -> Dict[
+    str, Any]:
     data = {
         "start_time_unix_nano": point.start_time_unix_nano,
         "time_unix_nano": point.time_unix_nano,
-        "attributes": parse_attributes(point.attributes),
-        "value": point.as_double if point.HasField("as_double") else point.as_int,
+        "attributes": __parse_attributes(point.attributes),
+        "value": point.as_double if point.HasField(
+            "as_double") else point.as_int,
         "flags": point.flags
     }
     return data
 
-def __parse_histogram_data_point(point: metrics_pb2.HistogramDataPoint) -> Dict[str, Any]:
+
+def __parse_histogram_data_point(point: metrics_pb2.HistogramDataPoint) -> Dict[
+    str, Any]:
     data = {
         "start_time_unix_nano": point.start_time_unix_nano,
         "time_unix_nano": point.time_unix_nano,
-        "attributes": parse_attributes(point.attributes),
+        "attributes": __parse_attributes(point.attributes),
         "count": point.count,
         "sum": point.sum if point.HasField("sum") else None,
         "bucket_counts": list(point.bucket_counts),
@@ -32,20 +41,25 @@ def __parse_histogram_data_point(point: metrics_pb2.HistogramDataPoint) -> Dict[
     }
     return data
 
-def __parse_summary_data_point(point: metrics_pb2.SummaryDataPoint) -> Dict[str, Any]:
+
+def __parse_summary_data_point(point: metrics_pb2.SummaryDataPoint) -> Dict[
+    str, Any]:
     data = {
         "start_time_unix_nano": point.start_time_unix_nano,
         "time_unix_nano": point.time_unix_nano,
-        "attributes": parse_attributes(point.attributes),
+        "attributes": __parse_attributes(point.attributes),
         "count": point.count,
         "sum": point.sum,
-        "quantile_values": {qv.quantile: qv.value for qv in point.quantile_values},
+        "quantile_values": {qv.quantile: qv.value for qv in
+                            point.quantile_values},
         "flags": point.flags
     }
     return data
 
+
 def metrics_resource2dataframe(metric: metrics_pb2.MetricsData) -> pd.DataFrame:
     pass
+
 
 def metrics_data2dataframe(metric: metrics_pb2.MetricsData) -> pd.DataFrame:
     rows = []
@@ -63,34 +77,43 @@ def metrics_data2dataframe(metric: metrics_pb2.MetricsData) -> pd.DataFrame:
                 }
                 if m.HasField("gauge"):
                     for point in m.gauge.data_points:
-                        row = {**common_data, **parse_number_data_point(point)}
+                        row = {**common_data, **__parse_number_data_point(
+                            point)}
                         rows.append(row)
                 elif m.HasField("sum"):
                     for point in m.sum.data_points:
-                        row = {**common_data, **parse_number_data_point(point)}
+                        row = {**common_data, **__parse_number_data_point(
+                            point)}
                         rows.append(row)
                 elif m.HasField("histogram"):
                     for point in m.histogram.data_points:
-                        row = {**common_data, **parse_histogram_data_point(point)}
+                        row = {**common_data,
+                               **__parse_histogram_data_point(point)}
                         rows.append(row)
                 elif m.HasField("exponential_histogram"):
                     for point in m.exponential_histogram.data_points:
-                        row = {**common_data, **parse_histogram_data_point(point)}
+                        row = {**common_data,
+                               **__parse_histogram_data_point(point)}
                         rows.append(row)
                 elif m.HasField("summary"):
                     for point in m.summary.data_points:
-                        row = {**common_data, **parse_summary_data_point(point)}
+                        row = {**common_data, **__parse_summary_data_point(
+                            point)}
                         rows.append(row)
     return pd.DataFrame(rows)
 
-def log_resource2dataframe(metric: logs_pb2.ResourceLogs) -> pd.dataframe:
+
+def log_resource2dataframe(metric: logs_pb2.ResourceLogs) -> pd.DataFrame:
     pass
 
-def log_data2dataframe(metric: logs_pb2.LogsData) -> pd.dataframe:
+
+def log_data2dataframe(metric: logs_pb2.LogsData) -> pd.DataFrame:
     pass
 
-def trace_resource2dataframe(metric: trace_pb2.ResourceSpans) -> pd.dataframe:
+
+def trace_resource2dataframe(metric: trace_pb2.ResourceSpans) -> pd.DataFrame:
     pass
 
-def trace_data2dataframe(metric: trace_pb2.TracesData) -> pd.dataframe:
+
+def trace_data2dataframe(metric: trace_pb2.TracesData) -> pd.DataFrame:
     pass
